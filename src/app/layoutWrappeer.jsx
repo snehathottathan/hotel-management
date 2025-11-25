91ccbeb53a0ac7084265eb33efa6816fd77bb8aa
Sneha <sneha@Snehas-MacBook-Air.local>
2025-11-25 07:54:30 +0530

    Initial commit

---
+++ src/app/layoutWrappeer.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Hero from "./pages/Hero/Hero";
+import Footer from "./pages/footer/Footer";
+
+export default function LayoutWrapper({ children }) {
+  const pathname = usePathname();
+
+  // Pages that should NOT show Hero + Footer
+ const noLayoutPages = ["/admin/login-page"];
+
+  console.log("noLayoutPages",noLayoutPages,pathname);
+  
+
+  const hideLayout = noLayoutPages.includes(pathname);
+
+  return (
+    <>
+      {!hideLayout && <Hero />}
+     <main style={{ flex: 1 }}>{children}</main>
+      {!hideLayout && <Footer />}
+    </>
+  );
+}
